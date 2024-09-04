0d173e38fdd8fa5e65cb052fdbaf28ed2b1ca022
Joyyang158 <hy2847@nyu.edu>
2024-09-03 23:13:05 -0400

    change the code

--- utils/preference_data.py
+++ utils/preference_data.py
@@ -2,38 +2,42 @@ import pandas as pd
 import os
 import json
 from datasets import load_dataset
+import shutil
 
-
-dataset_file_name = "gpt_score/Llama-2-7b-ultrachat200k/iter0.csv"
-df_real = pd.read_csv('gpt_score/Llama-2-7b-ultrachat200k/iter0.csv')
-df_generated = pd.read_csv(dataset_file_name)
-
-iter_n = dataset_file_name.split('/')[-1].replace(".csv","") 
+model_name = "zephyr-7b-sft-full"
+base_path = f"/blue/yonghui.wu/sgao1/haoyan"
+dataset_real_file_name = "iter0.csv"
+dataset_generated_file_name = "iter0.csv"
+df_real = pd.read_csv(f"{base_path}/data/gpt-score-{model_name}/{dataset_real_file_name}")
+df_generated = pd.read_csv(f"{base_path}/data/gpt-score-{model_name}/{dataset_generated_file_name}")
 
 data = []
 count_r = 0
 count_g = 0
 for i in range(len(df_real)):
-    if df_real['R_Score'][i] - df_generated['G_Score'][i] <= -5:
-        data.append({"real": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_real['R_Answer'][i])}], "generated": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_generated['G_Answer'][i])}]})
-        count_r += 1
-    else:
+    if df_generated['G_Score'][i] - df_real['R_Score'][i] >= 5:
         data.append({"real": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_generated['G_Answer'][i])}], "generated": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_real['R_Answer'][i])}]})
         count_g += 1
+    else:
+        data.append({"real": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_real['R_Answer'][i])}], "generated": [{"role": "user", "content": str(df_generated['Question'][i])}, {"role": "assistant", "content": str(df_generated['G_Answer'][i])}]})
+        count_r += 1
+        
 print(count_r)
 print(count_g)
 
-input_dir = f"generated-gpt-preference/Llama-2-7b-ultrachat200k/{iter_n}_v2"
-if not os.path.exists(f'{input_dir}/synthetic'):
-    os.makedirs(f'{input_dir}/synthetic')
+iter_n = dataset_generated_file_name.replace(".csv","") 
+output_dir  = f"{base_path}/gpt-preference-data/{model_name}/{iter_n}"
 
 
-with open(f'{input_dir}/synthetic/synthetic_train.json', 'w') as f:
+with open(f'{output_dir}/train.json', 'w') as f:
     json.dump(data, f, indent=4)
-# with open(f'{input_dir}/synthetic/synthetic_test.json', 'w') as f:
-#     json.dump(test_data, f, indent=4)
 
-dataset = load_dataset('json', data_files=f'{input_dir}/synthetic/synthetic_train.json',split='train')
-# dataset_test = load_dataset('json', data_files=f'{input_dir}/synthetic/synthetic_test.json',split='train')
+src_path = f"{base_path}/data/SPIN-generated-{model_name}/{iter_n}/test.json"
+dst_path = f"{output_dir}/test.json"
+shutil.copy(src_path, dst_path)
+
+
+dataset = load_dataset('json', data_files=f'{output_dir}/train.json',split='train')
+print(dataset)
 
 
