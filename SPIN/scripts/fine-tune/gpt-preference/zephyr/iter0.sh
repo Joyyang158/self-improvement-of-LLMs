d6a0d2d9d84e5137ce845c9ae0913d7ae05d8354
Joyyang158 <hy2847@nyu.edu>
2024-09-04 23:49:22 -0400

    change the code

--- SPIN/scripts/fine-tune/gpt-preference/zephyr/iter0.sh
+++ SPIN/scripts/fine-tune/gpt-preference/zephyr/iter0.sh
@@ -3,7 +3,7 @@
 # conda activate spinenv
 
 
-export CUDA_VISIBLE_DEVICES="5,6,7"
+export CUDA_VISIBLE_DEVICES="0,4,7"
 
 
 # Set the home directory for Hugging Face transformers library cache.
@@ -28,4 +28,4 @@ ACCELERATE_LOG_LEVEL=info
 # alignment-handbook/zephyr-7b-sft-full
 
 
-nohup accelerate launch --config_file SPIN/configs/deepspeed_zero3.yaml --num_processes=3 --main_process_port 2950 SPIN/spin/run_spin.py SPIN/configs/gpt-preference/zephyr/iter0.yaml --num_train_epochs=3 --output_dir="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/gpt-preference-outputs/iter0" > job_output.log 2>&1 &
+nohup accelerate launch --config_file SPIN/configs/deepspeed_zero3.yaml --num_processes=3 --main_process_port 2950 SPIN/spin/run_spin.py SPIN/configs/gpt-preference/zephyr/iter0.yaml --num_train_epochs=3 --output_dir="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/gpt-preference-5-outputs/iter0" > job_output.log 2>&1 &
