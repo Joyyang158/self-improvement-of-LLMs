63dac92c0188834e66ae4ae452fde75b4e62e276
Joyyang158 <hy2847@nyu.edu>
2024-09-06 10:45:15 -0400

    change the code

--- utils/eval.sh
+++ utils/eval.sh
@@ -1,6 +1,7 @@
-export CUDA_VISIBLE_DEVICES="1,4,5,6"
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 
-model="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/gpt-preference-10-outputs/iter0"
+# model="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/gpt-preference-10-outputs/iter0"
+model="joyfine/gpt-zephyr-7b-sft-full-SPIN-iter1"
 
 
 accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks arc_easy,arc_challenge --device cuda --batch_size 8 --num_fewshot 25
