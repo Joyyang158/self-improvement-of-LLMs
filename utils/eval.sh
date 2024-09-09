6ef516844257f258405d63cb6f344bfd06d07b3c
Joyyang158 <hy2847@nyu.edu>
2024-09-09 13:03:11 -0400

    change the code

--- utils/eval.sh
+++ utils/eval.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES="0,2,6"
 model="/blue/yonghui.wu/sgao1/haoyan/spin-results/Llama-2-7b-ultrachat200k/noised-gpt-preference-0-outputs/iter3/checkpoint-834"
 
 
-accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks arc_easy,arc_challenge --device cuda --batch_size 8 --num_fewshot 25
+# accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks arc_easy,arc_challenge --device cuda --batch_size 8 --num_fewshot 25
 accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks truthfulqa_mc1,truthfulqa_mc2 --device cuda --batch_size 8
 accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks winogrande --device cuda --batch_size 8 --num_fewshot 5
 accelerate launch /home/sgao1/haoyan/lm-evaluation-harness/lm_eval --model hf --model_args pretrained=$model,dtype='bfloat16' --tasks gsm8k --device cuda --batch_size 8 --num_fewshot 5
