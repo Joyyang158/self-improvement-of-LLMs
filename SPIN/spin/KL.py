90de27cc653bf292805973db3d45627332352138
Joyyang158 <hy2847@nyu.edu>
2024-09-13 15:55:22 -0400

    change the code

--- SPIN/spin/KL.py
+++ SPIN/spin/KL.py
@@ -50,7 +50,7 @@ df = pd.read_csv(f"{args.input_dir}/{args.input_file}")
 df_list = []
 
 for index, row in df.iterrows():
-    row_dict = {index: {'Question': row['Question'], 'R_answer': row['R_Answer'], 'G_answer': row['G_Answer']}}
+    row_dict = {index: {'Question': row['Question'], 'R_Answer': row['R_Answer'], 'G_Answer': row['G_Answer']}}
     df_list.append(row_dict)
 
 
