1c7b871669a4090a228467ae7ca4ea8889224dcc
Joyyang158 <hy2847@nyu.edu>
2024-09-13 15:26:38 -0400

    change the code

--- SPIN/spin/KL.py
+++ SPIN/spin/KL.py
@@ -13,6 +13,7 @@ import numpy as np
 import os
 import torch.nn.functional as F
 import random
+import pandas as pd
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -23,11 +24,13 @@ accelerator = Accelerator(kwargs_handlers=[kwargs])
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='/group-volume/haoyan/spin_results/zephyr-7b-sft-full/outputs/iter1-ckpt')
-parser.add_argument('--input_dir', type=str, default='generated/llama-7b/iter0/synthetic')
-parser.add_argument('--output_dir', type=str, default='sft_loss_test/llama-7b/iter0')
-parser.add_argument('--split', type=str, default='test')
-parser.add_argument('--data_type', type=str, default='real')
+parser.add_argument('--model', type=str, default='joyfine/zephyr-7b-sft-full-SPIN-iter0')
+parser.add_argument('--input_dir', type=str, default='/blue/yonghui.wu/sgao1/haoyan/data/gpt-score-zephyr-7b-sft-full')
+parser.add_argument('--input_file', type=str, default='iter0.csv')
+parser.add_argument('--output_dir', type=str, default='/blue/yonghui.wu/sgao1/haoyan/data')
+parser.add_argument('--output_file', type=str, default='iter0.csv')
+# parser.add_argument('--split', type=str, default='test')
+# parser.add_argument('--data_type', type=str, default='real')
 
 args = parser.parse_args()
 model_path = args.model
@@ -40,45 +43,54 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, truncation = True, max_length = 4096)  
 tokenizer.pad_token = tokenizer.eos_token
-data = load_dataset(args.input_dir, split=args.split)
-generated_data = data[args.data_type]
 
-### sample 100 - gpt
-# random.seed(42)
-# generated_data  = random.sample(generated_data, 100)
-###
+# data = load_dataset(args.input_dir, split=args.split)
+# generated_data = data[args.data_type]
+df = pd.read_csv(f"{args.input_dir}/{args.input_file}")
 
 loss_fn  = torch.nn.CrossEntropyLoss(ignore_index = tokenizer.pad_token_id, reduction = "none")
 
-df = pd.DataFrame(columns=['Question', 'R_Answer', 'G_Answer', 'R_Score', 'G_Score'])
+
+def calculate_token_logprob(question, answer):
+    model.eval()
+    tokenized_question = tokenizer(question, return_tensors = 'pt').to("cuda")
+    input_text = question + tokenizer.eos_token + answer
+    tokenized_input = tokenizer(input_text, return_tensors='pt').to("cuda")
+
+    output = model(**tokenized_input)
+    logits = output.logits
+
+    input_ids = tokenized_input['input_ids']
+    labels = input_ids[..., 1:]
+    shift_logits = logits[..., :-1, :]
+    log_probs = F.log_softmax(shift_logits, dim=-1)
+    target_log_probs = log_probs.gather(dim = -1, index = labels.unsqueeze(-1)).squeeze(-1)
+    final_log_probs = target_log_probs[:, tokenized_question['input_ids'].shape[-1] + 1:]
+    if len(final_log_probs) != 0:
+        avg_res = np.mean(final_log_probs)
+    else:
+        avg_res = 0
+    
+    return avg_res
+
+
+
 accelerator.wait_for_everyone()    
-with accelerator.split_between_processes(generated_data) as generated:
-    feature_informaion = []
-    log_prob_ls = []
-    for each in tqdm(generated):
-        model.eval()
-        question = each[0]['content']
-        answer = each[1]['content']
-        tokenized_question = tokenizer(question, return_tensors = 'pt').to("cuda")
-        input_text = question + tokenizer.eos_token + answer
-        tokenized_input = tokenizer(input_text, return_tensors='pt').to("cuda")
+with accelerator.split_between_processes(df) as data:
+    log_prob_dic = {}
+    for index, row in tqdm(data.iterrows()):
+        question = row['Question']
+        real_answer = row['R_Answer']
+        generated_answer = row['G_Answer']
 
-        output = model(**tokenized_input)
-        logits = output.logits
-
-        input_ids = tokenized_input['input_ids']
-        labels = input_ids[..., 1:]
-        shift_logits = logits[..., :-1, :]
-        log_probs = F.log_softmax(shift_logits, dim=-1)
-        target_log_probs = log_probs.gather(dim = -1, index = labels.unsqueeze(-1)).squeeze(-1)
-        sum_log_prob = target_log_probs[:, tokenized_question['input_ids'].shape[-1] + 1:].sum()
-        log_prob_ls.append(sum_log_prob.item())
-results_gathered_log_prob = gather_object(log_prob_ls)
-
-
+        avg_real_res = calculate_token_logprob(question, real_answer)
+        avg_generated_res = calculate_token_logprob(question, generated_answer)
+        log_prob_dic[index] = [avg_real_res, avg_generated_res]
+results_gathered_log_prob = gather_object(log_prob_dic)
 
 if accelerator.is_local_main_process:
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-    filename = f"{args.output_dir}/log_prob_{args.input_dir.split('/')[-2]}_{args.data_type}.npy"
-    np.save(filename, results_gathered_log_prob)
+    print(results_gathered_log_prob)
+    # if not os.path.exists(args.output_dir):
+    #     os.makedirs(args.output_dir)
+    # filename = f"{args.output_dir}/log_prob_{args.input_dir.split('/')[-2]}_{args.data_type}.npy"
+    # np.save(filename, results_gathered_log_prob)
