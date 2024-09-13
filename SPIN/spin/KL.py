6d1f31bf211b680c03de8c7e77e0b5464e35045d
Joyyang158 <hy2847@nyu.edu>
2024-09-13 16:15:21 -0400

    change the code

--- SPIN/spin/KL.py
+++ SPIN/spin/KL.py
@@ -74,7 +74,7 @@ def calculate_token_logprob(question, answer):
     final_log_probs = target_log_probs[:, tokenized_question['input_ids'].shape[-1] + 1:]
 
     if final_log_probs.shape[1] != 0:
-        avg_res = (final_log_probs.sum() / final_log_probs.shape[1]).item()
+        avg_res = np.round((final_log_probs.sum() / final_log_probs.shape[1]).item(),4)
     else:
         avg_res = 0
     
@@ -95,7 +95,6 @@ with accelerator.split_between_processes(df_list) as data:
         avg_real_res = calculate_token_logprob(question, real_answer)
         avg_generated_res = calculate_token_logprob(question, generated_answer)
         res_dic[index] = [avg_real_res, avg_generated_res]
-        print(res_dic)
         log_prob_ls.append(res_dic)
 results_gathered_log_prob = gather_object(log_prob_ls)
 
