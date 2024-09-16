2157f7f44712e3fb4c30c930f950403348ca4c22
Joyyang158 <hy2847@nyu.edu>
2024-09-15 21:20:28 -0400

    change the code

--- SPIN/scripts/fine-tune/gpt-preference-noise/zephyr/iter1.sh
+++ SPIN/scripts/fine-tune/gpt-preference-noise/zephyr/iter1.sh
@@ -3,7 +3,7 @@
 # conda activate spinenv
 
 
-export CUDA_VISIBLE_DEVICES="3,4,5,6"
+export CUDA_VISIBLE_DEVICES="0,2"
 
 
 # Set the home directory for Hugging Face transformers library cache.
@@ -28,4 +28,4 @@ ACCELERATE_LOG_LEVEL=info
 # alignment-handbook/zephyr-7b-sft-full
 
 
-nohup accelerate launch --config_file SPIN/configs/deepspeed_zero3.yaml --num_processes=4 --main_process_port 2950 SPIN/spin/run_spin.py SPIN/configs/gpt-preference-0/zephyr/iter1.yaml --num_train_epochs=3 --output_dir="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/trainable-noised-gpt-preference-0-outputs/iter1" > job_output_noise_iter1.log 2>&1 &
+nohup accelerate launch --config_file SPIN/configs/deepspeed_zero3.yaml --num_processes=2 --main_process_port 2950 SPIN/spin/run_spin.py SPIN/configs/gpt-preference-0/zephyr/iter1.yaml --num_train_epochs=3 --output_dir="/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/trainable-noised-gpt-preference-0-outputs/iter1" > job_output_noise_iter1.log 2>&1 &
