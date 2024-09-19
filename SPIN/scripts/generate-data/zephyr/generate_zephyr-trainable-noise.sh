9e8d1f4fa4404b61b9729103aa9cc4d65aba6ebb
Joyyang158 <hy2847@nyu.edu>
2024-09-18 23:19:38 -0400

    change the code

--- SPIN/scripts/generate-data/zephyr/generate_zephyr-trainable-noise.sh
+++ SPIN/scripts/generate-data/zephyr/generate_zephyr-trainable-noise.sh
@@ -3,9 +3,10 @@ export CUDA_VISIBLE_DEVICES=2,3,4,5
 FRAC_LEN=800
 TOTAL_RECORDS=20000
 
-NUM_BATCHES=$((TOTAL_RECORDS / FRAC_LEN))
+# NUM_BATCHES=$((TOTAL_RECORDS / FRAC_LEN))
+NUM_BATCHES=21
 
-for ((DATA_FRAC=0; DATA_FRAC < NUM_BATCHES; DATA_FRAC++))
+for ((DATA_FRAC=20; DATA_FRAC < NUM_BATCHES; DATA_FRAC++))
 do   
     echo "Train Data -- Processing batch $DATA_FRAC of $NUM_BATCHEs..."
     accelerate launch --num_processes 4 --main_process_port=2950 SPIN/spin/generate.py --model "/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/self-trainable-noise-gpt-preference-0-outputs/iter2" --batch_size 8 --frac_len $FRAC_LEN --data_frac $DATA_FRAC --output_dir /blue/yonghui.wu/sgao1/haoyan/data/trainable-noise-zephyr-7b-sft-full/iter3/train-raw-generated
@@ -15,13 +16,13 @@ echo "Train - All batches processed"
 
 
 
-for ((DATA_FRAC=0; DATA_FRAC < NUM_BATCHES; DATA_FRAC++))
-do 
-    echo "Test Data -- Processing batch $DATA_FRAC of $NUM_BATCHEs..."
-    accelerate launch --num_processes 4 --main_process_port=2950 SPIN/spin/generate.py --model "/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/self-trainable-noise-gpt-preference-0-outputs/iter2" --batch_size 8 --frac_len $FRAC_LEN --data_frac $DATA_FRAC --split test --output_dir /blue/yonghui.wu/sgao1/haoyan/data/trainable-noise-zephyr-7b-sft-full/iter3/test-raw-generated
+# for ((DATA_FRAC=0; DATA_FRAC < NUM_BATCHES; DATA_FRAC++))
+# do 
+#     echo "Test Data -- Processing batch $DATA_FRAC of $NUM_BATCHEs..."
+#     accelerate launch --num_processes 4 --main_process_port=2950 SPIN/spin/generate.py --model "/blue/yonghui.wu/sgao1/haoyan/spin-results/zephyr-7b-sft-full/self-trainable-noise-gpt-preference-0-outputs/iter2" --batch_size 8 --frac_len $FRAC_LEN --data_frac $DATA_FRAC --split test --output_dir /blue/yonghui.wu/sgao1/haoyan/data/trainable-noise-zephyr-7b-sft-full/iter3/test-raw-generated
 
-done
-echo "Test - All batches processed"
+# done
+# echo "Test - All batches processed"
 
 
 # accelerate launch --main_process_port=2950 spin/generate.py --model alignment-handbook/zephyr-7b-sft-full --input_dir UCLA-AGI/SPIN_iter0 --batch_size 8 --frac_len 800 --data_frac 1 --output_dir generated/iter0
