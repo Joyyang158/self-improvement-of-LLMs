a8eb1ec559ed6079546a3e2c039c45176334afc8
Joyyang158 <hy2847@nyu.edu>
2024-11-20 12:05:14 -0500

    change the code

--- SPIN/spin/alignment/trainer.py
+++ SPIN/spin/alignment/trainer.py
@@ -163,7 +163,7 @@ class SPINTrainer(Trainer):
             model.fc_logvar = nn.Sequential(
                 nn.LayerNorm(4096),
                 nn.GELU(),
-                nn.Linear(4096, 32000, bias = False)
+                nn.Linear(4096, 128256, bias = False)
             )
             # nn.init.zeros_(model.fc_logvar.weight)
 
@@ -577,7 +577,7 @@ class SPINTrainer(Trainer):
             trainable_gaussian_noise = torch.normal(0, 1, generated_logits.shape).to(f"cuda:{current_device_index}")
             no_trainable_gaussian_noise = torch.normal(0, 1, generated_logits.shape).to(f"cuda:{current_device_index}")
             var = self.model.fc_logvar(model_last_hidden_state).to(torch.float32)
-            exp_var = 0.25 * torch.exp(var)
+            exp_var = 0.5 * torch.exp(var)
             
             trainable_sample_noise = trainable_gaussian_noise * exp_var
             no_trainable_sample_noise = no_trainable_gaussian_noise * exp_var.detach()
