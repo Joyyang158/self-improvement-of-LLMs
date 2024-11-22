70153cc6100d8e5b0ab1bd635f1de096fea715e9
Joyyang158 <hy2847@nyu.edu>
2024-11-22 13:13:51 -0500

    change the code

--- SPIN/scripts/fine-tune/self-trainable-noise/llama3-8b/iter1.sh
+++ SPIN/scripts/fine-tune/self-trainable-noise/llama3-8b/iter1.sh
@@ -3,7 +3,7 @@
 # conda activate spinenv
 
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,6,7"
+export CUDA_VISIBLE_DEVICES="1,2,3,4,5,6,7"
 
 
 # Set the home directory for Hugging Face transformers library cache.
